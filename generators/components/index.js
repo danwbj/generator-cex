e69b9164664e18a07f2533f2dd5bf32921fcf430
吴丹丹 <danw@kailuzhe.cn>
2016-11-01 16:46:41 +0800

    component 生成器

---
+++ generators/components/index.js
@@ -0,0 +1,61 @@
+var generators = require('yeoman-generator')
+var _ = require('lodash')
+var shellby = require('shellby')
+
+var CEXGenerator = generators.Base.extend({
+    'initializing': function() {
+        console.log('initializing - cex')
+    },
+    'prompting': function() {
+        return this.prompt([
+            {
+                type: 'input',
+                name: 'component',
+                message: 'Your component name',
+                default: 'demo',
+                validate: function(input) {
+                    return input != ''
+                }
+            }, {
+                type: 'input',
+                name: 'author',
+                message: 'What\'s your name',
+                store: true
+            }
+        ]).then(function(answers) {
+            this.answers = answers
+        }.bind(this))
+    },
+    'configuring': function() {
+        console.log(this.answers)
+        console.log('configuring - cex')
+    },
+    'default': function() {
+        console.log('default - cex')
+    },
+    'writing': {
+        modules() {
+            var author = this.answers.author
+            var self = this
+            var component = {
+                Ab: _.upperFirst(this.answers.component),
+                ab: _.toLower(this.answers.component),
+                author: author
+            }
+            self.fs.copyTpl(self.templatePath('src/demo/demo.jsx'), self.destinationPath('src/components/' + component.ab+'/'+ component.ab + '.jsx'), component)
+            self.fs.copyTpl(self.templatePath('src/demo/demo.less'), self.destinationPath('src/components/' + component.ab+'/' + component.ab + '.less'), component)
+        }
+    },
+    'conflicts': function() {
+        console.log('conflicts - cex')
+    },
+    'install': function() {
+        // install dependencies
+        console.log('install - cex')
+    },
+    'end': function() {
+        console.log('end - cex')
+    }
+})
+
+module.exports = CEXGenerator
---
+++ generators/components/templates/src/demo/demo.jsx
@@ -0,0 +1,22 @@
+/**
+ * create by <%= author %>
+ */
+
+import React from 'react'
+import classNames from 'classnames'
+
+import './<%= ab %>.less'
+
+const <%= Ab %> = React.createClass({
+    render() {
+        return (
+            <div>
+               <%= ab %> component
+            </div>
+
+        )
+    }
+
+})
+
+export default <%= Ab %>
